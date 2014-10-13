2a0b20da372eb64b2c5470c2f599e13f3a665a5c
Schwarz <262747531@qq.com>
2014-10-13 18:10:33 +0800

    chrome.js modify

--- chrome.js
+++ chrome.js
@@ -1,20 +1,32 @@
 ﻿
-function getChannelData(date){
+function getChannelData(){
 
+        // $.ajax({
+        //         url: "http://www.baidu.com/",
+        //         xhrFields: {
+        //                 withCredentials: true
+        //         },
+        //         type: "GET",
+        //         data: {}
+        // }).done(function (log) {
+        //         console.log('selected Date: ', date);
+        //         console.log('channel amount: ', 33)
+        //         console.log('total revenue: ', 3333)
+        //         console.log(log);
+        // });
+        // return false;
         $.ajax({
-                url: "http://www.baidu.com/",
-                xhrFields: {
-                        withCredentials: true
-                },
+                url: "http://www.baidu.com",
+                xhrFields: { withCredentials: true },
                 type: "GET",
-                data: {}
-        }).done(function (log) {
-                console.log('selected Date: ', date);
-                console.log('channel amount: ', 33)
-                console.log('total revenue: ', 3333)
-                console.log(log);
+                data: {},
+                success: function(msg) {
+                        $("#lbdate").text('2014-10-12');
+                        $("#lbamount").text(33);
+                        $("#lbrevenue").text(3333);
+                        $(".txtcontent").text('ok!');
+                 }
         });
-        return false;
 }
 
 
@@ -24,5 +36,5 @@ function getChannelData(date){
 //document.body.insertBefore(jqueryScript, document.body.children.item(0));
 
 var div= document.createElement('div');
-div.innerHTML='<div>This is the Menu page</div><input type="text" id="goodplugin" class="goodplugin" /><button class="btn2">ick</button><br />Get logs:<br /><textarea style="width:300px; height:150px" class="txtcontent">some stuff</textarea><br />selected Date: <label id="lbdate"></label><br />channel amount: <label id="lbamount"></label><br />total revenue: <label id="lbrevenue"></label>';
+div.innerHTML='<div>This is the Menu page</div><input type="text" id="goodplugin" class="goodplugin" /><button class="btn2" onclick="getChannelData()">click</button><br />Get logs:<br /><textarea style="width:300px; height:150px" class="txtcontent">some stuff</textarea><br />selected Date: <label id="lbdate"></label><br />channel amount: <label id="lbamount"></label><br />total revenue: <label id="lbrevenue"></label>';
 document.body.insertBefore(div, document.body.children.item(1));
