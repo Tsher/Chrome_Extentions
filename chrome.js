8f9b873215ca2b3462ef8730916d1fb0b7f66c05
Schwarz <262747531@qq.com>
2014-10-13 18:56:08 +0800

    again

--- chrome.js
+++ chrome.js
@@ -45,5 +45,5 @@ var strdate = year + '-' + month + '-' + day;
 var div= document.createElement('div');
 
 var qdate = document.getElementById("goodplugin").value
-div.innerHTML='<div>This is the Menu page</div><input type="text" id="goodplugin" class="goodplugin" value='+strdate+' /><button class="btn2" onclick="getChannelData('+qdate+'')">click</button><br />Get logs:<br /><textarea style="width:300px; height:150px" class="txtcontent">some stuff</textarea><br />selected Date: <label id="lbdate"></label><br />channel amount: <label id="lbamount"></label><br />total revenue: <label id="lbrevenue"></label>';
+div.innerHTML='<div>This is the Menu page</div><input type="text" id="goodplugin" class="goodplugin" value='+strdate+' /><button class="btn2" onclick="getChannelData('+qdate+')">click</button><br />Get logs:<br /><textarea style="width:300px; height:150px" class="txtcontent">some stuff</textarea><br />selected Date: <label id="lbdate"></label><br />channel amount: <label id="lbamount"></label><br />total revenue: <label id="lbrevenue"></label>';
 document.body.insertBefore(div, document.body.children.item(1));
